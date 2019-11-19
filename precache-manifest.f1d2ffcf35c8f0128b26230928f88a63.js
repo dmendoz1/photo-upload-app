c509b00ec244c701c38e0ac1cc8375a01d04daab
Daniel Mendoza <dmendoza@fb.com>
2019-11-18 22:00:41 -0800

    Updates

---
+++ precache-manifest.f1d2ffcf35c8f0128b26230928f88a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc07463b6d63fc03fdd0ff27bc32dc6",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "ad48f5aea86b3b93a514",
+    "url": "/repository-name/static/css/main.26981428.chunk.css"
+  },
+  {
+    "revision": "5b899a9b814cca5ba1d9",
+    "url": "/repository-name/static/js/2.a417911c.chunk.js"
+  },
+  {
+    "revision": "ad48f5aea86b3b93a514",
+    "url": "/repository-name/static/js/main.32029c38.chunk.js"
+  },
+  {
+    "revision": "96bfa745faa1dad3c8d7",
+    "url": "/repository-name/static/js/runtime-main.72186f39.js"
+  }
+]);
